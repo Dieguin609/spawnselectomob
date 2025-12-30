2b8b2d64157e9a7faa42ac314e9b87e0a01e21c5
Dieguin609 <140520616+Dieguin609@users.noreply.github.com>
2025-12-30 13:41:14 -0300

    Update script.js

--- script.js
+++ script.js
@@ -1,20 +1,9 @@
-// script.js - EXCLUSIVO PARA MOBILE
 function selecionarSpawn(id) {
-    // Verifica se a API do Geckoju existe
     if (window.geckoju) {
-        // Envia os dados formatados em JSON para o servidor
+        // Formato JSON que o sscanf do Pawn acima vai ler
         window.geckoju.send(JSON.stringify({
             event: "server:selecionarSpawn",
             spawnId: id
         }));
-    } else {
-        console.log("Erro: Geckoju não detectado. Este arquivo é apenas para Mobile.");
     }
-}
-
-// Ouvinte caso o servidor queira enviar algo para a tela de Spawn
-if (window.geckoju) {
-    window.geckoju.onData = function(data) {
-        console.log("Dados recebidos do Pawn:", data);
-    };
 }
