0f2edfbe0fa75eea0c3c07ad034c6282bf52bfb1
Dieguin609 <140520616+Dieguin609@users.noreply.github.com>
2025-12-30 14:23:26 -0300

    Add files via upload

--- script.js
+++ script.js
@@ -1,8 +1,27 @@
-function selecionarSpawn(id) {
+// Função para o Botão 1: Última Posição
+function spawnUltima() {
     if (window.geckoju) {
-        // Envia o comando no formato: server:selecionarSpawn:ID
-        window.geckoju.send("server:selecionarSpawn:" + id);
-    } else {
-        console.log("Geckoju não detectado. Local: " + id);
+        window.geckoju.send("event:spawnUltima");
+    }
+}
+
+// Função para o Botão 2: Aeroporto
+function spawnAeroporto() {
+    if (window.geckoju) {
+        window.geckoju.send("event:spawnAeroporto");
+    }
+}
+
+// Função para o Botão 3: Estacionamento 1
+function spawnEstac1() {
+    if (window.geckoju) {
+        window.geckoju.send("event:spawnEstac1");
+    }
+}
+
+// Função para o Botão 4: Estacionamento 2
+function spawnEstac2() {
+    if (window.geckoju) {
+        window.geckoju.send("event:spawnEstac2");
     }
 }
