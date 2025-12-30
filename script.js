fee3331413ef1a3ba3b16b3ea560fbcc8f191879
Dieguin609 <140520616+Dieguin609@users.noreply.github.com>
2025-12-30 14:05:12 -0300

    Add files via upload

--- script.js
+++ script.js
@@ -1,9 +1,8 @@
 function selecionarSpawn(id) {
     if (window.geckoju) {
-        // Formato JSON que o sscanf do Pawn acima vai ler
-        window.geckoju.send(JSON.stringify({
-            event: "server:selecionarSpawn",
-            spawnId: id
-        }));
+        // Envia o comando no formato: server:selecionarSpawn:ID
+        window.geckoju.send("server:selecionarSpawn:" + id);
+    } else {
+        console.log("Geckoju não detectado. Local: " + id);
     }
 }
